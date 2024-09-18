615d48d73cd3fa1aa449210c102df8e54f04cd9c
JT-KLENKE <jtk96@cornell.edu>
2024-09-17 21:06:51 -0400

    add sponsors

--- src/pages/SponsorsPage.jsx
+++ src/pages/SponsorsPage.jsx
@@ -4,9 +4,11 @@ import './SponsorsPage.css';
 const sponsors = {
     "platinum": ["./images/onshape_logo.webp", "./images/simscale-logo.svg"],
     "gold": [],
-    "silver": [],
+    "silver": ["./images/Hargrave-logo.webp"],
     "bronze": [],
 }
+const special_thanks = ["./images/demonpower-logo.png", "./images/APD-logo.png", "./images/APC-logo.webp"]
+
 
 const SponsorsPage = () => {
     return (<>
@@ -58,6 +60,14 @@ const SponsorsPage = () => {
                 </>
             })
             }
+        </section>
+        <section className='sponsor-logos'>
+            <h2>And special thanks to...</h2>
+            <div className='logos'>
+                {special_thanks.map((img) => {
+                    return <div className="silver"><img className='sponsor-logo' src={img} /></div>
+                })}
+            </div>
         </section>
     </>)
 }
