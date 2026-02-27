102b9626c5f811bee12c7e4a4940b3569b5e07ea
CUAD <37247633+CUADProjectTeam@users.noreply.github.com>
2026-02-27 01:59:01 +0000

    Update

--- src/pages/MembersPage.jsx
+++ src/pages/MembersPage.jsx
@@ -98,7 +98,7 @@ const MembersPage = () => {
   const members = [
     { image: './images/Members/CUAD_Rilo.jpeg', name: 'Rilo Oberg', team: 'Full Team Lead' },
     { image: './images/Members/CUAD_John.JPG', name: 'John Apessos', team: 'Full Team  Lead' },
-    { image: './images/Members/CUAD_Lulu.JPG', name: 'Lujine Abdelfattah', team: 'Treasurer/Full Team Lead' }, //no pic
+    { image: './images/Members/CUAD_Lulu.JPG', name: 'Lujine Abdelfattah', team: 'Full Team Lead' }, //no pic
     { image: './images/Members/CUAD_AllieLin.jpeg', name: 'Allie Lin', team: 'Business Lead' },
     { image: './images/Members/CUAD_Jason.jpeg', name: 'Jason Peskin', team: 'Electrical Lead' },
     { image: './images/Members/CUAD_Abigail.jpeg', name: 'Abigail Gabalski', team: 'Electrical Lead' },
